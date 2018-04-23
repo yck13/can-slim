3fbccf51143db722eb97eb5479516b97028740d6
karl-chan <karlwayne_chan@yahoo.com>
2018-04-23 20:22:21 +0100

    Use fixtures

--- modules/scraper/lse_scraper_test.py
+++ modules/scraper/lse_scraper_test.py
@@ -1,17 +1,24 @@
-from modules.scraper.lse_scraper import LSEScraper
-from modules.core.model.index import Index
-from modules.core.model.index import Constituent
+import pytest
 
-def test_get_ftse_100_constituents():
-    scraper = LSEScraper()
+from modules.core.model.index import Constituent
+from modules.core.model.index import Index
+from modules.scraper.lse_scraper import LSEScraper
+
+
+@pytest.fixture
+def scraper():
+    return LSEScraper()
+
+
+def test_get_ftse_100_constituents(scraper):
     constituents = scraper.get_constituents(Index.FTSE100)
     barclays = Constituent(ticker='BARC', name='BARCLAYS', currency='GBX')
     gsk = Constituent(ticker='GSK', name='GLAXOSMITHKLINE', currency='GBX')
     assert barclays in constituents
     assert gsk in constituents
 
-def test_get_ftse_250_constituents():
-    scraper = LSEScraper()
+
+def test_get_ftse_250_constituents(scraper):
     constituents = scraper.get_constituents(Index.FTSE250)
     baillie = Constituent(ticker='BGFD', name='BAILLIE G.JAP.', currency='GBX')
     wizz_air = Constituent(ticker='WIZZ', name='WIZZ AIR', currency='GBX')
